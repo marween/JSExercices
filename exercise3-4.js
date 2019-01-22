10cf6b78e9a22d787770a11d2fb67bd84c3e47f1
marween <annemagali.saintgeorges@gmail.com>
2019-01-22 19:30:55 +0100

    ajout de l'exercice 3-4

---
+++ exercise3-4.js
@@ -0,0 +1,11 @@
+let number =  [2,6,9,41,39];
+
+console.log(Math.max(...number));
+console.log(Math.min(...number));
+
+// anciennement 
+//
+
+//let number=[5,7,11,9,1,2,120];
+//console.log(Math.max.apply(null, number));
+//console.log(Math.min.apply(null, number));
